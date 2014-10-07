91967abdd848c662cd59940e24f183edc80b4aed
Touhid Mia <hmtm.cse@gmail.com>
2014-10-07 11:54:56 +0600

    Touhid Mia

---
+++ js/properties.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Touhid Mia on 6/10/2014.
+ */
+
+console.log("Window or view port Width = " + jQuery(window).width())
+console.log("Window or view port Height = " + jQuery(window).height())
